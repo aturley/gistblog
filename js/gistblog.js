2ec68b7756bb0a8b91aaa9693e05ead23f8afc85
Andrew Turley <aturley@acm.org>
2012-02-28 21:15:58 -0500

    Fixes issue #10. http://user.github.com/gistblog/#GIST_ID now points to the post associated with GIST_ID.

--- js/gistblog.js
+++ js/gistblog.js
@@ -66,6 +66,29 @@ var renderBlogposts = function(blogposts) {
            });
 };
 
+var getGist = function(gistId) {
+    var url = "https://api.github.com/gists/" + gistId + "?callback=handleGist";
+    console.log("getGist(" + gistId + "), url: " + url);
+    $.getScript(url);
+};
+
+var handleGist = function(gist) {
+    console.log("handleGist()");
+    console.log("handling");
+    console.log(gist);
+    handleGists({data:[gist.data]});
+};
+
 $(document).ready(function(){
-                      getGists("aturley");
+                      console.log("window.location.pathname: " + window.location.href);
+                      if (window.location.href.indexOf("#") < 0) {
+                          console.log("get all posts");
+                          getGists("aturley");
+                      } else {
+                          console.log("get one post");
+                          var path = window.location.href;
+                          var gistId = path.substr(path.indexOf("#") + 1);
+                          console.log("get post " + gistId);
+                          getGist(gistId);
+                      }
                   });
